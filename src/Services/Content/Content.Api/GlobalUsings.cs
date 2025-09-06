b7fcf6283c8fef1da64ba5f4f37436ff88aecae2
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2025-09-06 09:28:33 -0700

    add global usings

---
+++ src/Services/Content/Content.Api/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using MediatR;
+global using Microsoft.AspNetCore.Mvc;
+global using Monster.BuildingBlocks;
+global using Monster.BuildingBlocks.Http;
---
+++ src/Services/Content/Content.Application/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using MediatR;
+global using FluentValidation;
+global using Monster.BuildingBlocks;
+global using Monster.Persistence.Abstractions;
